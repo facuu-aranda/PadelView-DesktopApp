33484a35a232e99c817a23e87f11273f391d1a3b
facuu-aranda <facu.aranda.grf@gmail.com>
2026-08-06 15:04:36 -0300

    fix: remove unused DeleteObjectCommand import

--- src/main/services/r2.service.ts
+++ src/main/services/r2.service.ts
@@ -1,4 +1,4 @@
-import { S3Client, GetObjectCommand, ListObjectsV2Command, ListObjectsV2CommandOutput, DeleteObjectsCommand, DeleteObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client, GetObjectCommand, ListObjectsV2Command, ListObjectsV2CommandOutput, DeleteObjectsCommand } from '@aws-sdk/client-s3'
 import { Upload } from '@aws-sdk/lib-storage'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { vaultService } from './vault.service'
